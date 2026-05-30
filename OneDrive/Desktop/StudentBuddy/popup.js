8ec8674f63169afb86f157ced46210dc8689bc07
Kavya <kavyaaparala2007@gmail.com>
2026-05-30 11:13:21 +0530

    Added delete task features

--- OneDrive/Desktop/StudentBuddy/popup.js
+++ OneDrive/Desktop/StudentBuddy/popup.js
@@ -1,11 +1,36 @@
-function addTask(){
+document.addEventListener("DOMContentLoaded", () => {
 
- let task=document.getElementById("taskInput").value;
+    const addBtn = document.getElementById("addBtn");
 
- let li=document.createElement("li");
+    addBtn.addEventListener("click", addTask);
 
- li.textContent=task;
+});
 
- document.getElementById("taskList").appendChild(li);
+function addTask() {
 
+    let task = document.getElementById("taskInput").value;
+
+    if (task.trim() === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    let taskText = document.createElement("span");
+    taskText.textContent = task;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    li.appendChild(taskText);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    document.getElementById("taskInput").value = "";
 }
