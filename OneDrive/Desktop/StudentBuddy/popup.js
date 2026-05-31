2aa5d901559736f16c6a590550eaea3f4715fec0
Kavya <kavyaaparala2007@gmail.com>
2026-05-31 09:31:15 +0530

    Fixed popup.js

--- OneDrive/Desktop/StudentBuddy/popup.js
+++ OneDrive/Desktop/StudentBuddy/popup.js
@@ -1,91 +1,60 @@
-function calculateCGPA(){
+document.addEventListener("DOMContentLoaded", () => {
 
-    let gpa1 =
-    parseFloat(
-        document.getElementById("gpa1").value
-    ) || 0;
+    document.getElementById("attendanceBtn")
+        .addEventListener("click", calculateAttendance);
 
-    let gpa2 =
-    parseFloat(
-        document.getElementById("gpa2").value
-    ) || 0;
+    document.getElementById("cgpaBtn")
+        .addEventListener("click", calculateCGPA);
 
-    let gpa3 =
-    parseFloat(
-        document.getElementById("gpa3").value
-    ) || 0;
+});
+
+function calculateCGPA() {
+
+    let gpa1 = parseFloat(document.getElementById("gpa1").value) || 0;
+    let gpa2 = parseFloat(document.getElementById("gpa2").value) || 0;
+    let gpa3 = parseFloat(document.getElementById("gpa3").value) || 0;
 
     let count = 0;
     let total = 0;
 
-    if(gpa1 > 0){
+    if (gpa1 > 0) {
         total += gpa1;
         count++;
     }
 
-    if(gpa2 > 0){
+    if (gpa2 > 0) {
         total += gpa2;
         count++;
     }
 
-    if(gpa3 > 0){
+    if (gpa3 > 0) {
         total += gpa3;
         count++;
     }
 
-    if(count === 0){
-
+    if (count === 0) {
         alert("Enter GPA values");
-
         return;
     }
 
     let cgpa = total / count;
 
-    document.getElementById(
-        "cgpaResult"
-    ).textContent =
-    "CGPA = " +
-    cgpa.toFixed(2);
+    document.getElementById("cgpaResult").textContent =
+        "CGPA = " + cgpa.toFixed(2);
 }
-function calculateAttendance(){
 
-    let attended =
-    parseFloat(
-        document.getElementById(
-            "attended"
-        ).value
-    );
+function calculateAttendance() {
 
-    let total =
-    parseFloat(
-        document.getElementById(
-            "total"
-        ).value
-    );
+    let attended = parseFloat(document.getElementById("attended").value);
+    let total = parseFloat(document.getElementById("total").value);
 
-    if(
-        isNaN(attended)
-        ||
-        isNaN(total)
-        ||
-        total === 0
-    ){
-
-        alert(
-            "Enter valid values"
-        );
-
+    if (isNaN(attended) || isNaN(total) || total === 0) {
+        alert("Enter valid values");
         return;
     }
 
-    let percentage =
-    (attended / total) * 100;
+    let percentage = (attended / total) * 100;
 
-    document.getElementById(
-        "attendanceResult"
-    ).textContent =
-    "Attendance = " +
-    percentage.toFixed(2)
-    + "%";
+    document.getElementById("attendanceResult").textContent =
+        "Attendance = " + percentage.toFixed(2) + "%";
 }
